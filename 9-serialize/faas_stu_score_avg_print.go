5b35f86252569da93a5abb45ca97dc9496b73e2f
aceld <danbing.at@gmail.com>
2024-04-07 11:04:19 +0800

    9-serialize

---
+++ 9-serialize/faas_stu_score_avg_print.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aceld/kis-flow/kis"
+	"github.com/aceld/kis-flow/serialize"
+)
+
+type PrintStuAvgScoreIn struct {
+	serialize.DefaultSerialize
+	StuScoreAvg
+}
+
+type PrintStuAvgScoreOut struct {
+	serialize.DefaultSerialize
+}
+
+func PrintStuAvgScore(ctx context.Context, flow kis.Flow, rows []*PrintStuAvgScoreIn) error {
+	fmt.Printf("->Call Func PrintStuAvgScore\n")
+
+	for _, row := range rows {
+		fmt.Printf("stuid: [%+v], avg score: [%+v]\n", row.StuId, row.AvgScore)
+	}
+
+	return nil
+}
